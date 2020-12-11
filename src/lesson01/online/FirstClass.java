4acde5e30340a957de2cb0e1aa41d342a03e5f06
toygildinpv <protagonyst@mail.ru>
2020-12-11 14:05:44 +0400

    created a training project

---
+++ src/lesson01/online/FirstClass.java
@@ -0,0 +1,21 @@
+package lesson01.online;
+
+public class FirstClass {
+
+    public static void main(String[] args){
+        System.out.println("Hello Java!!!");
+
+        byte vByte = 50; // 1 байт
+        short vShort = 500; // 2 байта
+        int vInt = 50000; // 4 байта
+        long vLong = 3000000000L; // 8 байт
+
+        float vFloat = 5.3F; // 4 байта
+        double vDouble = 0.2313; // 8 байт
+
+        char vChar = 'q'; // 2 байта
+
+        boolean vBoolean = false; // 1 байт
+    }
+
+}
