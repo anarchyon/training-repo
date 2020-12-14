1c24691af732af8f32f731dfc4db57c2b0825cb0
toygildinpv <protagonyst@mail.ru>
2020-12-15 00:20:27 +0400

    second lesson end

--- src/lesson2/online/Second.java
+++ src/lesson2/online/Second.java
@@ -1,2 +1,17 @@
-package lesson2.online;public class Second {
+package lesson2.online;
+
+public class Second {
+    public static void main(String[] args) {
+
+        printTablePifagor(6,4);
+    }
+
+    public static void printTablePifagor(int width, int heigth) {
+        for (int i = 1; i <= heigth; i++) {
+            for (int j = 1; j <= width; j++) {
+                System.out.print(i * j + "\t");
+            }
+            System.out.println("");
+        }
+    }
 }
